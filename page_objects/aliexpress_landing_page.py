3fbec38e6929d76dd58884a1688a696cdb1810ff
Outeyral94 <nicolasouteyral@gmail.com>
2020-01-12 14:11:58 -0300

    added property decorators in landing page. added popup managment logic

--- page_objects/aliexpress_landing_page.py
+++ page_objects/aliexpress_landing_page.py
@@ -8,14 +8,23 @@ class AliExpressLandingPage(BasePage):
 
     url = 'https://www.aliexpress.com/'
 
+    @property
     def search_input(self):
         element_path = 'search-key'
         by = By.ID
 
         return BaseElement(driver=self.driver, element_path=element_path, by=by)
 
+    @property
     def submit_button(self):
         element_path = ".//div[contains(@class, 'searchbar-operate-box')]/input"
+        by = By.XPATH
+
+        return BaseElement(driver=self.driver, element_path=element_path, by=by)
+
+    @property
+    def dialog_popup_close_button(self):
+        element_path = ".//a[contains(@class, 'close-layer')]"
         by = By.XPATH
 
         return BaseElement(driver=self.driver, element_path=element_path, by=by)
--- test_aliexpress.py
+++ test_aliexpress.py
@@ -13,10 +13,21 @@ class TestAliExpress(unittest.TestCase):
 
         self.chrome = webdriver.Chrome('drivers/chromedriver', chrome_options=options)
 
+        self.test_input_value = 'iPhone'
+
     def test_elements_sold(self):
 
         landing = AliExpressLandingPage(driver=self.chrome)
         landing.go()
+
+        try:
+            landing.dialog_popup_close_button.click()
+        except:
+            pass
+
+        landing.search_input.click()
+        landing.search_input.send_keys(self.test_input_value)
+        landing.submit_button.click()
 
     def tearDown(self):
 
